3244c0218be87660620dbf38112ff4f7c5c596cb
Adnan Adrianto <41867527+AdnanAdrianto@users.noreply.github.com>
2018-07-29 21:36:22 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+echo ' (+) Menginstall Paket (+)' 
+sleep 2
+pkg install vim
+pkg install nano
+pkg install neofetch
+pkg install figlet
+echo ' (+) Sukses Memasang (+)'
+sleep 2
---
+++ style.py
@@ -0,0 +1,23 @@
+clear
+echo '[+] Sedang Login...... [+]' | lolcat
+sleep 4
+clear
+echo '[+] Loading...[+]' | lolcat
+sleep 2
+clear
+echo [+] Sedang Mengumpulkan Data [+] | lolcat
+sleep 2
+clear
+figlet -f standard.flf "SUKSES" | lolcat
+sleep 5
+clear
+echo 'Author : AdnanAdrianto' | lolcat
+echo 'Team   : Defacer Tersakiti 404' | lolcat
+echo 'Date   : 29 -Juli- 2018' | lolcat
+echo 'Gmail  : adnanadrianto42@gmail.com' | lolcat
+echo ' --- Thank You For Your Support ---' | lolcat
+sleep 5
+clear
+echo 'root@kali' | lolcat
+neofetch | lolcat
+date | lolcat
